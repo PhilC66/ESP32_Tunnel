9d82173fbbd2a0a1cf207d358730707503fbeb4c
PhilC66 <moncasot@gmail.com>
2019-02-16 15:39:11 +0100

    retour n pedale >=1, log extinction

--- ESP32_Tunnel.ino
+++ ESP32_Tunnel.ino
@@ -435,19 +435,19 @@ void loop() {
     }
   }
 
-  if (IRQ_Cpt_PDL1 > 1 || IRQ_Cpt_PDL2 > 1) {
+  if (IRQ_Cpt_PDL1 > 0 || IRQ_Cpt_PDL2 > 0) {
     Serial.print(F("Interruption : "));
     Serial.print(IRQ_Cpt_PDL1);
     Serial.print(" ");
     Serial.println(IRQ_Cpt_PDL2);
   }
-  if (IRQ_Cpt_PDL1 > 1) {
+  if (IRQ_Cpt_PDL1 > 0) {
     Allumage(1);
     portENTER_CRITICAL(&mux);
     IRQ_Cpt_PDL1 = 0;
     portEXIT_CRITICAL(&mux);
   }
-  if (IRQ_Cpt_PDL2 > 1) {
+  if (IRQ_Cpt_PDL2 > 0) {
     Allumage(2);
     portENTER_CRITICAL(&mux);
     IRQ_Cpt_PDL2 = 0;
@@ -2068,15 +2068,15 @@ void Allumage(byte n) {
     if (n == 0) {
       digitalWrite(PinEclairage, LOW);
       Allume = false;
+			Sbidon  = F("Extinction ");
+			Sbidon += n;
+			MajLog(F("Auto"), Sbidon);
     }
     else if (Al1 == Cd2 || Al2 == Cd1) {
       Serial.print(F("Extinction dans (s) ")), Serial.println(config.tempoSortie);
       Alarm.enable(TempoSortie);
       Serial.print(F("Nombre Allumage = ")), Serial.println(CptAllumage);
     }
-    Sbidon  = F("Extinction ");
-    Sbidon += n;
-    MajLog(F("Auto"), Sbidon);
   }
 }
 //---------------------------------------------------------------------------
